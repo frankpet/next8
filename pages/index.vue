d6aa9d16f0e66981dd3698332b399a358bb2a8e1
F Pet <fpet@mbp-van-f.home>
2020-12-23 12:49:29 +0100

    lorum

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="">
-    Hello nuxt app
+  <div class="container">
+    <h1>Hello nuxt app</h1>
+    <p>Wat is Lorem Ipsum?
+Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken. Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting. Het is in de jaren '60 populair geworden met de introductie van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software zoals Aldus PageMaker die versies van Lorem Ipsum bevatten.</p>
   </div>
 </template>
 
@@ -13,7 +15,7 @@ export default {
     }
   },
   mounted() {
-    
+
   },
   methods: {}
 }
